6268ed97ed5e1a5e92161ad7ffa7bcae44763b6c
Alepan <darenagel@hotmail.it>
2022-07-18 07:56:56 +0200

    first commit

---
+++ experiment.php
@@ -0,0 +1,12 @@
+<?php
+    echo 2 + 2;
+    $myname = 'John Doe';
+?>
+
+<h1>This page is alla baout <?php echo $myname ?></h1>
+
+<?php
+    echo 5 * 5
+?>
+
+<h2>All about <?php echo $myname ?></h2>
---
+++ wp-content/themes/fictional-university-theme/footer.php
@@ -0,0 +1,5 @@
+<p>Greetings from header.php</p>
+
+<?php wp_footer();?>
+</body>
+</html>
