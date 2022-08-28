b50c8feec673f64e28f149dc02860aadcc157440
Alepan <darenagel@hotmail.it>
2022-08-28 10:38:43 +0200

    custom post type program

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -19,24 +19,33 @@ function university_features()
 
 add_action('after_setup_theme', 'university_features');
 
-function univerisity_adjust_queries($query) {
+function univerisity_adjust_queries($query)
+{
+
+    if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('posts_per_page', -1);
+    }
+
+
     /* if you are not in the admin side
     if you are on the archive event page
     if query that you are manipulating is the url based one
     than modifiy it */
-    if (!is_admin() AND is_post_type_archive(('event')) AND $query->is_main_query()) {
+    if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
         $today = date('Ymd');
-        $query->set('meta_key','event_date');
-        $query->set('orderby','meta_value_num');
-        $query->set('order','ASC');
-        $query->set('meta_query',array(
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
             array(
-              'key' => 'event_date',
-              'compare' => '>=',
-              'value' => $today,
-              'type' => 'numeric'
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
             )
-          ));
+        ));
     }
 }
 
